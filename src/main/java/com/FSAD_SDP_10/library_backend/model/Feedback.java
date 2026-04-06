8f5f4f6256de5f911fc9e4947d2055e55ba4c2b8
saikoushik087 <j.saikoushik08@gmail.com>
2026-04-06 18:52:37 +0530

    feat: add Feedback entity for student reviews

---
+++ src/main/java/com/FSAD_SDP_10/library_backend/model/Feedback.java
@@ -0,0 +1,69 @@
+package com.FSAD_SDP_10.library_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long bookId;
+    private String studentName;
+    private String comment;
+    private int rating;
+
+    public Feedback() {
+    }
+
+    public Feedback(Long bookId, String studentName, String comment, int rating) {
+        this.bookId = bookId;
+        this.studentName = studentName;
+        this.comment = comment;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
