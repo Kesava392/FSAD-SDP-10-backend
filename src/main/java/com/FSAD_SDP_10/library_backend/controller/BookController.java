4ed6ede2bb0adb3f4076bd347aeae6466b5070c4
Kesava392 <nnkesavakumar21@gmail.com>
2026-04-28 14:38:59 +0530

    Update CrossOrigin annotation to allow all origins

--- src/main/java/com/FSAD_SDP_10/library_backend/controller/BookController.java
+++ src/main/java/com/FSAD_SDP_10/library_backend/controller/BookController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Autowired
